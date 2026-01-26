7ba8fcecbe1d47eff04a8074e150afaebe897842
QuanghocIT2k4 <ndnquang3072004@gmail.com>
2026-01-26 21:54:00 +0700

    Fix: Change Order import to org.springframework.core.annotation.Order

--- src/main/java/com/cinema/config/DatabaseConfig.java
+++ src/main/java/com/cinema/config/DatabaseConfig.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Order;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
 
 import javax.sql.DataSource;
 
